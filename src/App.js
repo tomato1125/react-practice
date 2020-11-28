998f8d6df723b57a21bbd2fa0c78dc73bcaa9d8b
tomato1125 <naokiyoshida1125@icoud.com>
2020-11-28 10:45:08 +0900

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+        <br/>
+        <br/>
+        <p>
+          <b>フィットネス・エンジニアの勉強会</b>
+        </p>
+        <a
+          className="App-link"
+          href="https://yu-sei-m.github.io/react-hands-on"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          はじめて学ぶReact
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
